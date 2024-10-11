a2add1635f02ab7448f28604441a32cabbdc8e5d
theeunsu <theeunsu@gmail.com>
2024-10-11 18:02:25 +0200

    parsing map

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -6,19 +6,51 @@
 /*   By: eahn <eahn@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 12:02:09 by eahn              #+#    #+#             */
-/*   Updated: 2024/10/09 17:20:18 by eahn             ###   ########.fr       */
+/*   Updated: 2024/10/11 16:47:40 by eahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* malloc, free, exit */
+#ifndef CUB3D_H
+# define CUB3D_H
+
 #include "libft.h"
+
+/* malloc, free, exit */
 #include <stdlib.h>
+
+/* open */
+#include <fcntl.h>
+
 #include <unistd.h>
+
+
+typedef struct s_img
+{
+	
+} t_img;
+
+typedef struct s_map
+{
+	int width;
+	int height;
+	char **grid; // 2D array
+} t_map;
+
+typedef struct s_player
+{
+	int x;
+	int y;		
+} t_player;
 
 typedef struct s_game
 {
 	void	*mlx;
 	void	*win;
+	t_img	img;
+	t_map	map;
+	t_player player;
 }			t_game;
 
 void		print_error(char *message);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: eahn <eahn@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 15:28:59 by eahn              #+#    #+#             */
-/*   Updated: 2024/10/09 17:19:40 by eahn             ###   ########.fr       */
+/*   Updated: 2024/10/11 16:48:19 by eahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,51 @@ static void check_args (int ac, char **av)
 }
 
 
+void parse_map(char *file, t_game *game)
+{
+    int fd;
+    int ret;
+    char *line;
+    
+
+    fd = open(file, O_RDONLY);
+    if (fd < 0)
+        print_error("Failed to open file.\n");
+    fd = get_next_line(fd, &line);
+    while (fd > 0)
+    {
+        if (ft_strncmp(line, "NO ", 3) == 0 || ft_strcmp(line, "SO ") == 0 || ft_strcmp(line, "WE ") == 0 || ft_strcmp(line, "EA ") == 0)
+            parse_direction(&game->map, line); // TBI
+        else if (ft_strncmp(line, "F ", 2) == 0 || ft_strncmp(line, "C ", 2) == 0)
+            parse_color(&game->map, line, line[0]); // TBI
+        else
+            parse_map_grid(&game->map, &game->player, line); // TBI
+        free(line);
+        ret = get_next_line(fd, &line);
+    }
+    close(fd);
+}
+
+void init_struct(t_game *game)
+{
+    game->mlx = NULL;
+    game->win = NULL;
+    game->img.space = NULL;
+    game->img.wall = NULL;
+    game->img.player = NULL;
+    game->img.exit = NULL;
+    game->img.collec = NULL;
+    game->map.width = 0;
+    game->map.height = 0;
+    game->map.grid = NULL;
+}
+
+
 int main (int ac, char **av)
 {
-    t_game *game;
+    t_game game;
 
     check_args (ac, av);
-    read_map(av[1]);
-    
+    init_struct(&game);
+    parse_map(av[1], &game);
 }
